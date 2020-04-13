149472b532b92fc29a41e8e605fe9a55ca623965
Richell Bonfield <richell.bonfield12@gmail.com>
2020-04-13 04:30:01 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
 
-const store = createStore(shoppingListItemReducer); /* code change */
+const store = createStore(shoppingListItemReducer); /* code change */const store = createStore(
+  shoppingListItemReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+); /* code change */
 
 ReactDOM.render(
   <Provider store={store}>
